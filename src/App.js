78fe0d994a3722917571a69d328973bf613e3803
Daniel ten Napel <daniel.ten.napel@capgemini.com>
2023-10-04 10:40:07 +0200

    used custom contentstack fetch hook in app.js

--- src/App.js
+++ src/App.js
@@ -8,31 +8,23 @@ import Homepage from "./pages/Homepage";
 import Allnewspage from "./pages/Allnewspage";
 import Singlenewspage from "./pages/Singlenewspage";
 import Faqpage from "./pages/Faqpage";
+import useContentStackFetch from "./utils/useContentStackFetch";
 
 export default function App() {
-  const [settings, setSettings] = useState(null);
+  const fetchOptions = {
+    contentType: "global_settings",
+    references: [],
+    jsonToHTML: [],
+  };
 
-  useEffect(() => {
-    const Stack = Contentstack.Stack({
-      api_key: process.env.REACT_APP_API_KEY,
-      delivery_token: process.env.REACT_APP_DELIVERY_TOKEN,
-      environment: process.env.REACT_APP_ENVIROMENT,
-      region: Contentstack.Region.EU,
-    });
-    const Query = Stack.ContentType("global_settings").Query();
-    Query.language("en-us")
-      .toJSON()
-      .find()
-      .then((result) => setSettings(result[0][0]))
-      .catch((error) => console.error(error));
-  }, []);
+  const [settings, isLoading, isError] = useContentStackFetch(fetchOptions);
 
-  const slogan = settings?.slogan ?? "";
-  const showBanner = settings?.banner?.display_banner ?? false;
-  const bannerLabel = settings?.banner?.banner_label ?? "";
-  const bannerDialog = settings?.banner?.banner_dialog ?? "";
-  const bannerColor = settings?.banner?.banner_color ?? "#ffffff";
-  const information = settings?.copyright_information ?? "";
+  const slogan = settings?.[0].slogan ?? "";
+  const showBanner = settings?.[0].banner?.display_banner ?? false;
+  const bannerLabel = settings?.[0].banner?.banner_label ?? "";
+  const bannerDialog = settings?.[0].banner?.banner_dialog ?? "";
+  const bannerColor = settings?.[0].banner?.banner_color ?? "#ffffff";
+  const information = settings?.[0].copyright_information ?? "";
 
   return (
     <BrowserRouter>
